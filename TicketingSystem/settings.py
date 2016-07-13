9fea1dfdf956340bfa9880628c4209fe873d7be5
Shiv Konar <shiv.konar@outlook.com>
2016-07-13 16:12:08 +0100

    Update settings.py

--- TicketingSystem/settings.py
+++ TicketingSystem/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'shiva.konar@gmail.com'
-EMAIL_HOST_PASSWORD = 'Saibaba@1987'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
